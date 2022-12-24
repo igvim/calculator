7ba66c6fb702c1951e64bee921032bf0eba3b146
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-24 15:41:24 -0500

    successfully adding display values and operators to expressions array

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+/*
+enter a number, pushing to array
+when an operator is clicked, store array and operator
+when button is next clicked, clear display and start new display array, 
+only after operator has been clicked
+- how to identify operator has been clicked?
+*/
+
 const display = document.querySelector('.display');
 const numKeys = document.querySelectorAll('.number');
-const opKeys = document.querySelectorAll('.op');
+const opKeys = Array.from(document.querySelectorAll('.op'));
 let displayVal = [];
 let operand = '';
 let expression = [];
 
 numKeys.forEach(numKey => {
     numKey.addEventListener('click', (e) => {
+        const pressedKey = getPressedKey(opKeys);
+        if (pressedKey) {
+            displayVal = [];
+            clearDisplay();
+            pressedKey.classList.toggle('pressed');
+        }
         const keyValue = e.target.textContent;
         updateDisplay(keyValue);
         displayVal.push(keyValue);
@@ -15,11 +29,12 @@ numKeys.forEach(numKey => {
 
 opKeys.forEach(opKey => {
     opKey.addEventListener('click', (e) => {
+        opKey.classList.toggle('pressed');
         if (!displayVal.length) return;
         operand = displayVal.join('');
-        const opVal = e.target.textContent;
-        expression.push(operand, opVal);
-        opKey.classList.toggle('pressed');
+        expression.push(operand);
+        const operator = e.target.textContent;
+        expression.push(operator);
     })
 })
 
@@ -60,4 +75,12 @@ function updateDisplay(val) {
     displayNum.classList.add('display-value');
     displayNum.textContent = val;
     display.appendChild(displayNum);
+}
+
+function clearDisplay() {
+    while (display.firstChild) display.removeChild(display.firstChild);
+}
+
+function getPressedKey(keysArr) {
+    return keysArr.find(key => key.classList.length > 2);
 }
